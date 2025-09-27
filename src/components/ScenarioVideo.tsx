dd57a60ca33c582490883ad20d5052e71f9596d7
m3ru <merugopalan2007@gmail.com>
2025-09-27 18:54:34 -0400

    new bill

--- src/components/ScenarioVideo.tsx
+++ src/components/ScenarioVideo.tsx
@@ -6,7 +6,7 @@ export default function ScenarioVideo({ scenario }: { scenario: Scenario }) {
   return (
     <div className="flex-grow flex flex-col justify-end" style={{ maxHeight: 'calc(100vh - 3rem)' }}>
       <video loop autoPlay muted>
-        <source src="/BillLoop.mp4" type="video/mp4" />
+        <source src="/BillyLoop.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
     </div>
