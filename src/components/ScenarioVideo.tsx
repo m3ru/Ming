4711a5acad9288883df4eaaf8041d95cd894871c
m3ru <merugopalan2007@gmail.com>
2025-09-27 16:27:13 -0400

    keep bill

--- src/components/ScenarioVideo.tsx
+++ src/components/ScenarioVideo.tsx
@@ -6,7 +6,7 @@ export default function ScenarioVideo({ scenario }: { scenario: Scenario }) {
   return (
     <div className="flex-grow flex flex-col justify-end">
       <video loop autoPlay muted>
-        <source src="/WillLoop.mp4" type="video/mp4" />
+        <source src="/BillLoop.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
     </div>
