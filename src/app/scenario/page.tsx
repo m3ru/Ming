67a6fa610e763ae3c23980453e80478b5e68c53f
Meru Gopalan <meru@lawn-128-61-4-159.lawn.gatech.edu>
2025-09-26 22:06:35 -0400

    ChatPanel addition

--- src/app/scenario/page.tsx
+++ src/app/scenario/page.tsx
@@ -1,5 +1,10 @@
+'use client'
+
 import ScenarioOverview from "@/components/ScenarioOverview";
 import { Scenarios } from "@/lib/scenarios";
+import { SidePanelCedarChat } from "@/cedar/components/chatComponents/SidePanelCedarChat";
+import SimpleChatPanel from "@/components/SimpleChatPanel";
+
 
 const scenario = Scenarios.demandingClient;
 
@@ -10,6 +15,11 @@ export default function ScenarioPage() {
       <div className="flex-grow bg-gray-200 flex flex-col">
         <div className="w-full p-2 text-2xl font-bold text-center bg-white">
           Scenario: {scenario.title}
+        </div>
+      </div>
+      <div className="w-[350px] flex flex-col items-end">
+        <div className="h-[60vh] p-4 bg-white rounded shadow w-full">
+          <SimpleChatPanel />
         </div>
       </div>
     </div>
